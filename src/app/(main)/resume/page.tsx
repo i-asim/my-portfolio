b021e6bc609aaf7558d2f30a5b54935aeb07c131
asim02i <aasimibr@gmail.com>
2025-12-01 12:26:57 +0530

    resume loading issue

--- src/app/(main)/resume/page.tsx
+++ src/app/(main)/resume/page.tsx
@@ -1,63 +1,7 @@
-import ResumeViewerWithFallback from "@/app/(main)/resume/resume-viewer-fallback";
-import { domainPath, personalInfo } from "@/constants";
-import { Metadata } from "next";
-import ResumeDocument from "./resume-document";
+"use client";
 
-export const metadata: Metadata = {
-  title: `${personalInfo.name} | Resume`,
-  description:
-    "Professional resume of Asim I, showcasing experience, skills, and education.",
-  keywords: [
-    "resume",
-    "Asim I",
-    "web developer",
-    "professional resume",
-    "experience",
-    "skills",
-    "education",
-  ],
-  authors: [{ name: personalInfo.name, url: domainPath }],
-  creator: personalInfo.name,
-  openGraph: {
-    title: `${personalInfo.name} | Resume`,
-    description:
-      "Professional resume of Asim I, showcasing experience, skills, and education.",
-    url: `${domainPath}/resume`,
-    siteName: `${personalInfo.name}'s Resume`,
-    images: [
-      {
-        url: "/images/projects/portfolio.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Resume | Asim I",
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    title: `${personalInfo.name} | Resume`,
-    description:
-      "Professional resume of Asim I, showcasing experience, skills, and education.",
-    images: ["/images/projects/portfolio.jpg"],
-    card: "summary_large_image",
-    creator: personalInfo.name,
-  },
-  alternates: {
-    canonical: `${domainPath}/resume`,
-  },
-  other: {
-    "application/ld+json": JSON.stringify({
-      "@context": "https://schema.org",
-      "@type": "Resume",
-      name: `${personalInfo.name}'s Resume`,
-      url: `${domainPath}/resume`,
-      author: {
-        "@type": "Person",
-        name: personalInfo.name,
-      },
-    }),
-  },
-};
+import ResumeViewerWithFallback from "@/app/(main)/resume/resume-viewer-fallback";
+import ResumeDocument from "./resume-document";
 
 export default function ResumePage() {
   return (
